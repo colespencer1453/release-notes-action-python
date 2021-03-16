fe45210e11e4b2ee5288c409d581483dbd1b24d6
colespencer1453 <css.csce1453@gmail.com>
2021-03-16 11:46:25 -0500

    Testing sections

--- main.py
+++ main.py
@@ -9,6 +9,32 @@ def main():
     myTeamsMessage = pymsteams.connectorcard(os.environ["INPUT_TEAMSWEBHOOK"])
     myTeamsMessage.title(str(my_input['release']['name']))
     myTeamsMessage.text(my_input['release']['body'])
+    
+
+    # create the section
+    myMessageSection = pymsteams.cardsection()
+
+    # Section Title
+    myMessageSection.title("Section title")
+
+    # Activity Elements
+    myMessageSection.activityTitle("my activity title")
+    myMessageSection.activitySubtitle("my activity subtitle")
+    myMessageSection.activityImage("http://i.imgur.com/c4jt321l.png")
+    myMessageSection.activityText("This is my activity Text")
+
+    # Facts are key value pairs displayed in a list.
+    myMessageSection.addFact("this", "is fine")
+    myMessageSection.addFact("this is", "also fine")
+
+    # Section Text
+    myMessageSection.text("This is my section text")
+
+    # Section Images
+    myMessageSection.addImage("http://i.imgur.com/c4jt321l.png", ititle="This Is Fine")
+
+    # Add your section to the connector card object before sending
+    myTeamsMessage.addSection(myMessageSection)
     myTeamsMessage.send()
 
 
