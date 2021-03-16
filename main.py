4cd03217b41ae5ebfe7685f441d5342c511e47ea
colespencer1453 <css.csce1453@gmail.com>
2021-03-16 11:58:10 -0500

    edited message sent

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def main():
     my_input = json.loads(os.environ["INPUT_RELEASEPAYLOAD"])
     print(my_input)
     myTeamsMessage = pymsteams.connectorcard(os.environ["INPUT_TEAMSWEBHOOK"])
-    myTeamsMessage.title(str(my_input['release']['name']))
+    myTeamsMessage.title("Release notes for version " + str(my_input['release']['tag_name']) +"of repository: " + str(my_input['repository']['name']))
     myTeamsMessage.text(my_input['release']['body'])
     
 
@@ -15,23 +15,11 @@ def main():
     myMessageSection = pymsteams.cardsection()
 
     # Section Title
-    myMessageSection.title("Section title")
+    myMessageSection.title(str(my_input['release']['name']))
 
-    # Activity Elements
-    myMessageSection.activityTitle("my activity title")
-    myMessageSection.activitySubtitle("my activity subtitle")
-    myMessageSection.activityImage("http://i.imgur.com/c4jt321l.png")
-    myMessageSection.activityText("This is my activity Text")
-
-    # Facts are key value pairs displayed in a list.
-    myMessageSection.addFact("this", "is fine")
-    myMessageSection.addFact("this is", "also fine")
 
     # Section Text
-    myMessageSection.text("This is my section text")
-
-    # Section Images
-    myMessageSection.addImage("http://i.imgur.com/c4jt321l.png", ititle="This Is Fine")
+    myMessageSection.text(str(my_input['release']['body'])
 
     # Add your section to the connector card object before sending
     myTeamsMessage.addSection(myMessageSection)
