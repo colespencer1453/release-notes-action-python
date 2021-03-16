e3db57a5580a91ca1803e32ecd105ee408ac8854
colespencer1453 <80274122+colespencer1453@users.noreply.github.com>
2021-03-16 11:29:38 -0500

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import json
 def main():
     my_input = json.loads(os.environ["INPUT_MYINPUT"])
     print(my_input)
-    myTeamsMessage = pymsteams.connectorcard("https://appriver3651001066.webhook.office.com/webhookb2/59c2a62d-30d9-498b-b5c9-add98d4bbb07@52934c9b-912c-4480-b2bc-72fee70477bb/IncomingWebhook/1bafa1bd08fa4298ad8d07f5ebe3af51/a07dc6c5-7106-49f1-88ea-3bfd14382686")
+    myTeamsMessage = pymsteams.connectorcard(os.environ["INPUT_TEAMSWEBHOOK"])
     myTeamsMessage.title(str(my_input['release']['name']))
     myTeamsMessage.text('hello')
     myTeamsMessage.send()
